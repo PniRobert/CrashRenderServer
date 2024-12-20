d79041d9bd8c798ea0a3b73174a74b46b4ea9131
Robert Li <rli@pniazure.com>
2024-12-20 11:13:01 -0800

    backup

--- CrashRenderServer/Program.cs
+++ CrashRenderServer/Program.cs
@@ -2,60 +2,73 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace HttpClientFactoryDemo
+var serviceCollection = new ServiceCollection();
+ConfigureServices(serviceCollection);
+var serviceProvider = serviceCollection.BuildServiceProvider();
+var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+
+var settings = new AppSettings();
+configuration.GetSection("AppSettings").Bind(settings);
+
+var filePath = "TestUrls.txt";
+var testUrls = new List<string>();
+
+try
 {
-    class Program
+    using (StreamReader reader = new StreamReader(filePath))
     {
-        static void Main(string[] args)
-        {            
-            var serviceCollection = new ServiceCollection();
-            ConfigureServices(serviceCollection);            
-            var serviceProvider = serviceCollection.BuildServiceProvider();
-            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
-
-            var settings = new AppSettings();
-            configuration.GetSection("AppSettings").Bind(settings);
-
-            if (settings.NumberOfThread.HasValue && !string.IsNullOrWhiteSpace(settings.TargetRenderServerDomainUri))
-            {
-                StartWork(serviceProvider, settings.NumberOfThread.Value, settings.TargetRenderServerDomainUri);
-            }
-            else
-            {
-                Console.WriteLine($"Settings have issue/s, target: {settings.TargetRenderServerDomainUri}\tnumber of thread: {settings.NumberOfThread}");
-            }
-
-            Console.WriteLine("Hi enter to exit program");
-            Console.ReadLine();
-        }
-
-        private static void ConfigureServices(IServiceCollection services)
+        string? line;
+        while ((line = reader.ReadLine()) != null)
         {
-            // Add configuration
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(AppContext.BaseDirectory) 
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)                 
-                .Build(); 
-            services.AddSingleton<IConfiguration>(configuration);
-            services.AddHttpClient();
+            testUrls.Add(line);
         }
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"An error occurred: {e.Message}");
+}
 
-        private static void StartWork(ServiceProvider serviceProvider, int threadNumber, string targetDomainUrl)
+if (settings.NumberOfThread.HasValue && !string.IsNullOrWhiteSpace(settings.TargetRenderServerDomainUri))
+{
+    StartWork(serviceProvider, settings.NumberOfThread.Value, settings.TargetRenderServerDomainUri, testUrls);
+}
+else
+{
+    Console.WriteLine($"Settings have issue/s, target: {settings.TargetRenderServerDomainUri}\tnumber of thread: {settings.NumberOfThread}");
+}
+
+Console.WriteLine("Hi enter to exit program");
+Console.ReadLine();
+
+void ConfigureServices(IServiceCollection services)
+{
+    // Add configuration
+    var configuration = new ConfigurationBuilder()
+        .SetBasePath(AppContext.BaseDirectory)
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .Build();
+    services.AddSingleton<IConfiguration>(configuration);
+    services.AddHttpClient();
+}
+
+void StartWork(ServiceProvider serviceProvider, int threadNumber, string targetDomainUrl, IEnumerable<string> testUrls)
+{
+    for (var i = 0; i < threadNumber; i++)
+    {
+        Task.Run(async () =>
         {
-            for (var i = 0; i < threadNumber; i++)
+            while (true)
             {
-                Task.Run(async () =>
+                foreach(var url in testUrls)
                 {
-                    while (true)
-                    {
-                        var testUrl = $"{targetDomainUrl}/api/project/t/1evOX30uKcR2f86ypwQt9dxJewZGdNLLhpX4A7FQTw782VrBLboBpFn1,pVPaDEcm6PkeygjRFQUUQfuTl,a1oST_zvaHjk6tV8O,IJIM154-/hires/19.jpg?checkSum=1";
-                        Console.WriteLine(testUrl);
-                        var httpClient = serviceProvider.GetRequiredService<IHttpClientFactory>().CreateClient();
-                        var response = await httpClient.GetAsync(testUrl);
-                        _ = await response.Content.ReadAsByteArrayAsync();
-                    }
-                });
+                    var testUrl = $"{targetDomainUrl}{url}";
+                    Console.WriteLine(testUrl);
+                    var httpClient = serviceProvider.GetRequiredService<IHttpClientFactory>().CreateClient();
+                    var response = await httpClient.GetAsync(testUrl);
+                    _ = await response.Content.ReadAsByteArrayAsync();
+                }
             }
-        }
+        });
     }
 }
