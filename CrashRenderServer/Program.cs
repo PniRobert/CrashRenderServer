07e0a1d7c53502bee4eb207489469c4b060c192e
rli <rli@pnimedia.com>
2024-12-23 18:06:00 -0800

    backup

--- CrashRenderServer/Program.cs
+++ CrashRenderServer/Program.cs
@@ -12,6 +12,7 @@ configuration.GetSection("AppSettings").Bind(settings);
 
 var filePath = "TestUrls.txt";
 var testUrls = new List<string>();
+CancellationTokenSource source = new CancellationTokenSource();
 
 try
 {
@@ -23,23 +24,25 @@ try
             testUrls.Add(line);
         }
     }
+
+    if (settings.NumberOfThread.HasValue && !string.IsNullOrWhiteSpace(settings.TargetRenderServerDomainUri))
+    {
+        StartWork(serviceProvider, settings.NumberOfThread.Value, settings.TargetRenderServerDomainUri, testUrls, source.Token);
+    }
+    else
+    {
+        Console.WriteLine($"Settings have issue/s, target: {settings.TargetRenderServerDomainUri}\tnumber of thread: {settings.NumberOfThread}");
+    }
 }
 catch (Exception e)
 {
     Console.WriteLine($"An error occurred: {e.Message}");
 }
 
-if (settings.NumberOfThread.HasValue && !string.IsNullOrWhiteSpace(settings.TargetRenderServerDomainUri))
-{
-    StartWork(serviceProvider, settings.NumberOfThread.Value, settings.TargetRenderServerDomainUri, testUrls);
-}
-else
-{
-    Console.WriteLine($"Settings have issue/s, target: {settings.TargetRenderServerDomainUri}\tnumber of thread: {settings.NumberOfThread}");
-}
-
 Console.WriteLine("Hi enter to exit program");
 Console.ReadLine();
+source.Cancel();
+source.Dispose();
 
 void ConfigureServices(IServiceCollection services)
 {
@@ -52,21 +55,30 @@ void ConfigureServices(IServiceCollection services)
     services.AddHttpClient();
 }
 
-void StartWork(ServiceProvider serviceProvider, int threadNumber, string targetDomainUrl, IEnumerable<string> testUrls)
+void StartWork(ServiceProvider serviceProvider, int threadNumber, string targetDomainUrl, IEnumerable<string> testUrls, CancellationToken token)
 {
     for (var i = 0; i < threadNumber; i++)
     {
         Task.Run(async () =>
         {
+            var httpClient = serviceProvider.GetRequiredService<IHttpClientFactory>().CreateClient();
             while (true)
             {
                 foreach(var url in testUrls)
                 {
                     var testUrl = $"{targetDomainUrl}{url}";
                     Console.WriteLine(testUrl);
-                    var httpClient = serviceProvider.GetRequiredService<IHttpClientFactory>().CreateClient();
-                    var response = await httpClient.GetAsync(testUrl);
-                    _ = await response.Content.ReadAsByteArrayAsync();
+                    try
+                    {
+                        var response = await httpClient.GetAsync(testUrl, token);
+                        _ = await response.Content.ReadAsByteArrayAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Run into error: {ex.Message}");
+                        Console.WriteLine(ex.StackTrace);
+                    }
+                    Thread.Sleep(500);
                 }
             }
         });
